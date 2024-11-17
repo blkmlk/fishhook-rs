a0535a5023ab0f6f89071dedea5d1f5f95290387
Islam Bekbuzarov <n157318@gmail.com>
2024-11-17 23:14:36 +0300

    removed libc and object

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,14 @@
-extern crate libc;
-
 use crate::arch::{MachHeaderT, NlistT, SegmentCommandT};
 use goblin::mach::constants::{
     SECTION_TYPE, SEG_DATA, SEG_LINKEDIT, S_LAZY_SYMBOL_POINTERS, S_NON_LAZY_SYMBOL_POINTERS,
 };
 use goblin::mach::load_command::{LC_DYSYMTAB, LC_SYMTAB};
-use libc::{c_char, c_int, c_void, dladdr, uintptr_t, Dl_info};
 use mach2::kern_return::KERN_SUCCESS;
 use mach2::traps::mach_task_self;
 use mach2::vm::mach_vm_protect;
 use mach2::vm_prot::{VM_PROT_COPY, VM_PROT_READ, VM_PROT_WRITE};
 use mach2::vm_types::{mach_vm_address_t, mach_vm_size_t};
-use object::{Object, ObjectSymbolTable};
-use std::ffi::CStr;
+use std::ffi::{c_char, c_int, c_void, CStr};
 use std::ptr::{null, null_mut};
 
 static mut BINDINGS: Vec<Rebinding> = Vec::new();
@@ -21,33 +17,41 @@ const SEG_DATA_CONST: &str = "__DATA_CONST";
 
 #[cfg(target_pointer_width = "64")]
 mod arch {
-    use goblin::mach::load_command::{Section64, SegmentCommand64};
+    use goblin::mach::header::Header64;
+    use goblin::mach::load_command::{Section64, SegmentCommand64, LC_SEGMENT_64};
     use goblin::mach::symbols::Nlist64;
-    use libc::mach_header_64;
 
-    pub const LC_SEGMENT_ARCH_DEPENDENT: u32 = libc::LC_SEGMENT_64;
+    pub const LC_SEGMENT_ARCH_DEPENDENT: u32 = LC_SEGMENT_64;
     pub type NlistT = Nlist64;
     pub type SectionT = Section64;
     pub type SegmentCommandT = SegmentCommand64;
-    pub type MachHeaderT = mach_header_64;
+    pub type MachHeaderT = Header64;
 }
 
 #[cfg(target_pointer_width = "32")]
 mod arch {
-    use goblin::mach::load_command::{Section32, SegmentCommand32};
+    use goblin::mach::header::Header32;
+    use goblin::mach::load_command::{Section32, SegmentCommand32, LC_SEGMENT, LC_SEGMENT_64};
     use goblin::mach::symbols::Nlist32;
-    use libc::mach_header;
-    use object::macho::Section64;
 
-    pub const LC_SEGMENT_ARCH: u32 = libc::LC_SEGMENT;
+    pub const LC_SEGMENT_ARCH_DEPENDENT: u32 = LC_SEGMENT;
     pub type NlistT = Nlist32;
     pub type SectionT = Section32;
     pub type SegmentCommandT = SegmentCommand32;
-    pub type MachHeaderT = mach_header;
+    pub type MachHeaderT = Header32;
+}
+
+#[repr(C)]
+pub struct Dl_info {
+    pub dli_fname: *const c_char,
+    pub dli_fbase: *mut c_void,
+    pub dli_sname: *const c_char,
+    pub dli_saddr: *mut c_void,
 }
 
 extern "C" {
     fn _dyld_register_func_for_add_image(callback: extern "C" fn(*const c_void, c_int));
+    fn dladdr(header: *const c_void, dl_info: *mut Dl_info) -> c_int;
 }
 
 #[derive(Clone)]
@@ -73,65 +77,63 @@ unsafe fn rebind_for_image(header: *const c_void, slide: c_int) {
         return;
     };
 
-    let mut linked_segment = None;
+    let mut linked_segment_cmd = None;
     let mut symtab_cmd = None;
-    let mut dysymtab = None;
+    let mut dynsymtab_cmd = None;
 
     let header = header as *const MachHeaderT;
 
-    let mut cur_seg_cmd =
-        header.byte_add(size_of::<MachHeaderT>() as uintptr_t) as *const arch::SegmentCommandT;
+    let mut segment_cmd = header.byte_add(size_of::<MachHeaderT>()) as *const SegmentCommandT;
 
     for _ in 0..(*header).ncmds {
-        match (*cur_seg_cmd).cmd {
+        match (*segment_cmd).cmd {
             arch::LC_SEGMENT_ARCH_DEPENDENT => {
-                if eq_u8((*cur_seg_cmd).segname, SEG_LINKEDIT) {
-                    linked_segment = Some(cur_seg_cmd);
+                if eq_u8((*segment_cmd).segname, SEG_LINKEDIT) {
+                    linked_segment_cmd = Some(segment_cmd);
                 }
             }
             LC_SYMTAB => {
-                symtab_cmd = Some(cur_seg_cmd as *const goblin::mach::load_command::SymtabCommand);
+                symtab_cmd = Some(segment_cmd as *const goblin::mach::load_command::SymtabCommand);
             }
             LC_DYSYMTAB => {
-                dysymtab = Some(cur_seg_cmd as *const goblin::mach::load_command::DysymtabCommand);
+                dynsymtab_cmd =
+                    Some(segment_cmd as *const goblin::mach::load_command::DysymtabCommand);
             }
             _ => {}
         }
 
-        cur_seg_cmd = cur_seg_cmd.byte_add((*cur_seg_cmd).cmdsize as uintptr_t);
+        segment_cmd = segment_cmd.byte_add((*segment_cmd).cmdsize as usize);
     }
 
-    let (Some(symtab_cmd), Some(dysymtab)) = (symtab_cmd, dysymtab) else {
+    let (Some(symtab_cmd), Some(dynsymtab_cmd)) = (symtab_cmd, dynsymtab_cmd) else {
         return;
     };
 
-    if (*dysymtab).nindirectsyms == 0 {
+    if (*dynsymtab_cmd).nindirectsyms == 0 {
         return;
     }
 
-    let symbol_table = header.byte_add((*symtab_cmd).symoff as uintptr_t) as *const arch::NlistT;
+    let symbol_table = header.byte_add((*symtab_cmd).symoff as usize) as *const NlistT;
     let indirect_symbol_table =
-        header.byte_add((*dysymtab).indirectsymoff as uintptr_t) as *const u32;
+        header.byte_add((*dynsymtab_cmd).indirectsymoff as usize) as *const u32;
 
-    let mut segment_cmd =
-        header.byte_add(size_of::<MachHeaderT>() as uintptr_t) as *const arch::SegmentCommandT;
+    let mut segment_cmd = header.byte_add(size_of::<MachHeaderT>()) as *const SegmentCommandT;
 
     for _ in 0..(*header).ncmds {
         if (*segment_cmd).cmd != arch::LC_SEGMENT_ARCH_DEPENDENT {
-            segment_cmd = segment_cmd.byte_add((*segment_cmd).cmdsize as uintptr_t);
+            segment_cmd = segment_cmd.byte_add((*segment_cmd).cmdsize as usize);
             continue;
         }
 
         if !eq_u8((*segment_cmd).segname, SEG_DATA)
             && !eq_u8((*segment_cmd).segname, SEG_DATA_CONST)
         {
-            segment_cmd = segment_cmd.byte_add((*segment_cmd).cmdsize as uintptr_t);
+            segment_cmd = segment_cmd.byte_add((*segment_cmd).cmdsize as usize);
             continue;
         }
 
         for j in 0..(*segment_cmd).nsects {
-            let sect = segment_cmd
-                .byte_add(size_of::<SegmentCommandT>() as uintptr_t + j as uintptr_t)
+            let sect = segment_cmd.byte_add(size_of::<SegmentCommandT>() + j as usize)
                 as *const arch::SectionT;
 
             if (*sect).flags & SECTION_TYPE != S_NON_LAZY_SYMBOL_POINTERS
@@ -140,10 +142,9 @@ unsafe fn rebind_for_image(header: *const c_void, slide: c_int) {
                 continue;
             }
 
-            let indirect_bindings =
-                ((*sect).addr as uintptr_t + slide as uintptr_t) as *mut *const c_void;
+            let indirect_bindings = ((*sect).addr as usize + slide as usize) as *mut *const c_void;
 
-            'symbol_loop: for k in 0..(*dysymtab).nindirectsyms {
+            'symbol_loop: for k in 0..(*dynsymtab_cmd).nindirectsyms {
                 let symbol_index = *indirect_symbol_table.add(k as usize);
 
                 if symbol_index >= (*symtab_cmd).nsyms {
@@ -152,7 +153,7 @@ unsafe fn rebind_for_image(header: *const c_void, slide: c_int) {
 
                 let symbol = symbol_table.add(symbol_index as usize) as *const NlistT;
                 let symbol_name = header
-                    .byte_add((*symtab_cmd).stroff as uintptr_t + (*symbol).n_strx as uintptr_t)
+                    .byte_add((*symtab_cmd).stroff as usize + (*symbol).n_strx as usize)
                     as *const c_char;
 
                 let name = CStr::from_ptr(symbol_name).to_string_lossy().to_string();
@@ -186,7 +187,7 @@ unsafe fn rebind_for_image(header: *const c_void, slide: c_int) {
             }
         }
 
-        segment_cmd = segment_cmd.byte_add((*segment_cmd).cmdsize as uintptr_t);
+        segment_cmd = segment_cmd.byte_add((*segment_cmd).cmdsize as usize);
     }
 }
 
